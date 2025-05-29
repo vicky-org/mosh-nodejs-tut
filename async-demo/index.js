ff2efa037a453a1dc05ca6ccfbbacf64ac738a33
Vignesh Krishnaraj <vkrishnaraj@atlassian.com>
2025-05-29 21:41:27 +0530

    consuming promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,71 +1,48 @@
 console.log('Before');
 
-// callbackFunction = (user) => {
-//     console.log('User:', user);
-// };
+//Follow this pattern to avoid callback hell - using Promises
+getUser(1)
+    .then(user => getRepositories(user))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('Commits: ', commits))
+    .catch(err => console.error('Error:', err.message));
 
-// function callbackFunction(user) {
-//     console.log('User:', user);
-// };
-
-//getUser(1, callbackFunction);
-
-// getUser(1, (user) => {
-//     console.log('User:', user);
-// });
-
-//Asynchronous JavaScript with Callbacks
-// getUser(1, (user) => {
-//     getRepositories(user, (repos) => {
-//         getCommits(repos[0], (commits) => {
-//             //This is callback hell, since we have nested callbacks
-//             console.log('Commits:', commits);
-//         });
-//     }
-//     );
-// });
-
-//Follow this pattern to avoid callback hell - Names fucntions to the rescue:
-getUser(1, getRepositories);
-
-
-
-function getRepositories(user) {
-    console.log('User:', user);
-    getRepositories(user, getCommits)
-}
-
-function getCommits(repos) {
-     console.log('Repositories:', repos);
-    getCommits(repos[0], displayCommits)
-}
-
-
-function displayCommits(commits) {
-    console.log('Commits:', commits);
-}
 
 console.log('After');
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading user from database...');
-        callback({ id: id, name: 'John Doe' })
-    }, 2000);
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        // Simulate a database call
+        setTimeout(() => {
+            console.log('Reading user from database...');
+            const user = { id: id, name: 'John Doe' };
+            resolve(user);
+            //reject(new Error('User not found'));
+        }, 2000);
+    })
 }
 
 
-function getRepositories(user, callback)  {
-    setTimeout(() => {
-        console.log(`Getting repositories for user... ${user.name}`);
-        console.log(callback);
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
+function getRepositories(user)  {
+    return new Promise((resolve, reject) => {
+        // Simulate a database call to get repositories
+        setTimeout(() => {
+            console.log(`Getting repositories for user... ${user.name}`);
+            const repos = ['repo1', 'repo2', 'repo3'];
+            resolve(repos);
+            //reject(new Error('Repositories not found'));
+        }, 2000);
+    });
 }
 
-function getCommits(repo, callback)  {
-    setTimeout(() => {
-        console.log(`Getting commits in repo... ${repo}`);
-        callback(['commit1', 'commit2', 'commit3']);
-    }, 2000);
+function getCommits(repo)  {
+    return new Promise((resolve, reject) => {
+        // Simulate a database call to get commits
+        setTimeout(() => {
+            console.log(`Getting commits in repo... ${repo}`);
+            const commits = ['commit1', 'commit2', 'commit3'];
+            resolve(commits);
+            //reject(new Error('Commits not found'));
+        }, 2000);
+    });
 }
