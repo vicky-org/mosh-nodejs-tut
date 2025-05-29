09305c0b6a2d63c4bb404d9175a77c878521298a
Vignesh Krishnaraj <vkrishnaraj@atlassian.com>
2025-05-29 20:44:50 +0530

    2nd lesson

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,9 +1,41 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('After setTimeout - Before nested setTimeout');
-    setTimeout(() => {
-        console.log('After nested setTimeout');
-    }, 2000);
-    console.log('After setTimeout - After nested setTimeout');
-}, 2000);
+
+// callbackFunction = (user) => {
+//     console.log('User:', user);
+// };
+
+// function callbackFunction(user) {
+//     console.log('User:', user);
+// };
+
+//getUser(1, callbackFunction);
+
+// getUser(1, (user) => {
+//     console.log('User:', user);
+// });
+
+getUser(1, (user) => {
+    console.log('User:', user);
+
+    getRepositories(user, (repos) => {
+        console.log('Repositories:', repos);
+    }
+    );
+});
+
 console.log('After');
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading user from database...');
+        callback({ id: id, name: 'John Doe' })
+    }, 2000);
+}
+
+
+function getRepositories(user, callback)  {
+    setTimeout(() => {
+        console.log(`Getting repositories for user... ${user.name}`);
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
